5e0e9af893ecb8b8bc1e612d8671204b69bbfdf2
Mos152 <moschellasebastian@gmail.com>
2021-04-25 00:43:50 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const socket = io();
+
+
+//document.addEventListener("DOMContentLoaded",() =>{    
+//      const b1 = document.getElementById("number1");
+//      b1.addEventListener("click", () => {
+//        let content = b1.textContent + 0 + 0 + 0;
+//        console.log(content);
+//      })
+//    })
---
+++ server.js
@@ -0,0 +1,43 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const five = require("johnny-five");
+const board = new five.Board()
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+io.on('connection', (socket) => {
+  board.on("ready",function(){
+    var ledA = new five.Led(13);
+    var ledB = new five.Led(12);
+    var ledC = new five.Led(11);
+    socket.on('ledA', (boolean) => {
+      //console.log("se prendio el led A",boolean)
+      if(boolean){
+        ledA.on();
+      }else{     
+        ledA.off();
+      }
+    })
+    socket.on('ledB', (boolean) => {
+      //console.log("se prendio el led A",boolean)
+      if(boolean){
+        ledB.on();
+      }else{     
+        ledB.off();
+      }
+    })
+    socket.on('ledC', (boolean) => {
+      //console.log("se prendio el led A",boolean)
+      if(boolean){
+        ledC.on();
+      }else{     
+        ledC.off();
+      }
+    })
+  })
+});
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
